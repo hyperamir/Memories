077b88a6c0dcea4c328fa06f1dc032b6eb31af3c
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-01 17:31:22 -0400

    create env file

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://Amir-memo:1234@cluster0.pxkrr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4001 ;
 
-const PORT = 4000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
   }).catch(error => console.log(error.message));
