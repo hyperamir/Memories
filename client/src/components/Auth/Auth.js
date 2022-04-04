1d63655b1805a0ac9c65f79ef69a6b027bf3dd0e
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-04 13:52:19 -0400

    create signup/signin page

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { Avatar, Button, Paper, Grid, Container, Typography } from "@material-ui/core";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from "./Input";
+import GoogleLogin from 'react-google-login';
+import Icon from './Icon';
 
 const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleShowPassword = () => setShowPassword(prev => !prev);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  const switchMode = () => {
+    setIsSignUp(prev => !prev);
+    handleShowPassword(false);
+  }
+
+  const googleSuccess = async (res) => {
+    console.log(res);
+  }
+
+  const googleFailure = () => {
+    console.log('Google Login Was Unsuccessful. Please Try Again!');
+  }
+
   return (
-    <div>
-      Auth
-    </div>
+    <Container component="main" maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+                <Input name='firstName' label='First Name' handleChange={handleChange} half />
+              </>
+            )}
+            <Input name='email' label='Email Address' handleChange={handleChange} type='email' />
+            <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            {isSignUp && <Input name='confirmPassword' label='Retype Password' handleChange={handleChange} type='password' />}
+          </Grid>
+          <Button className={classes.submit} type="submit" fullWidth variant="contained" color='primary'>
+            {isSignUp ? 'Sign Up' : 'Sign In'}
+          </Button>
+          <GoogleLogin
+            clientId="539244999987-6eau5cb4a2p0nlpem6fuarsitcgplrqo.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
+
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignUp ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   );
 }
 
