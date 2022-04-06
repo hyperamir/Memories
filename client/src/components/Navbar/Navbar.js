85ae4e70cc6f33fe46d22edc5a354cf4205cc01a
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-06 15:50:24 -0400

    show delete/edit button only for current user

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import useStyles from './styles';
 import memories from '../../images/memories.png';
 import { useDispatch } from "react-redux";
 import { LOGOUT } from "../../constants/actionTypes";
+import decode from 'jwt-decode';
 
 const Navbar = () => {
   const classes = useStyles();
@@ -14,6 +15,11 @@ const Navbar = () => {
 
   useEffect(() => {
     const token = user?.token;
+
+    if(token){
+      const decodedToken = decode(token);
+      if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
 
