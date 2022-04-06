42db606fc84bc7e0e72f4ec2a31195fb46a4f9de
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-04-06 18:27:50 -0400

    create rout for postdetail

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 function App() {
 
@@ -12,7 +13,10 @@ function App() {
       <Container maxWidth="lg">
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Navigate replace to='/posts' />} />
+          <Route path='/posts' element={<Home />} />
+          <Route path='/posts/search' element={<Home />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
           <Route path='/auth' element={<Auth />} />
         </Routes>
       </Container>
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostDetails = () => {
+  console.log('postdetailsss')
+  return (
+    <div>
+      post details
+    </div>
+  );
+}
+
+export default PostDetails;
